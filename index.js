01688d3e8a6bb440b00d65d07364165718d8a65f
Milton <miltonduarte2289@gmail.com>
2022-04-03 14:28:42 -0300

    Inicio del Proyecto

---
+++ index.js
@@ -0,0 +1,19 @@
+fetch('https://jsonplaceholder.typicode.com/users/')
+.then(res => res.json())
+.then(data => mostrarDatos(data))
+.catch(err => console.log(err))
+
+const mostrarDatos = (data) => {
+
+    console.log(data);
+
+    let body = '';
+
+    for (let i = 0; i < data.length; i++) {
+
+        body += `<tr><th>${data[i].id}</th><th>${data[i].name}</th><th>${data[i].email}</th></tr>`;
+        
+    }
+
+    document.getElementById('tbody').innerHTML = body;
+}
